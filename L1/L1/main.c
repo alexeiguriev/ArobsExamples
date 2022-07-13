0ac4d26e5cb2e8cc07af96dea4cba1fc758d543f
alexeiguriev <59488277+alexeiguriev@users.noreply.github.com>
2022-07-13 09:32:15 +0300

    Lesson 1 commit

---
+++ L1/L1/main.c
@@ -0,0 +1,34 @@
+/*
+ * L1.c
+ *
+ * Created: 7/12/2022 4:32:54 PM
+ * Author : alexei.guriev
+ */ 
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PD4
+#define BUTTON PD0
+
+int main(void)
+{
+    /* Replace with your application code */
+	
+	DDRD = (0x01 << LED);     //Configure the PORTD4 as output
+	DDRD &= ~ (0x01 << BUTTON);     //Configure the PORTD4 as output
+    while (1) 
+    {
+		if (PIND & (0x01 << BUTTON))
+		{
+			PORTD |= 1 << LED;
+		}
+		else
+		{
+			PORTD &= ~(1 << LED);
+		}
+	    //PORTD ^= (1 << LED);
+		//_delay_us(1000000);
+    }
+}
+
